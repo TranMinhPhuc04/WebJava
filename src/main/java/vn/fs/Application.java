99dc5d5c829b197dbb302a8ae2e8729e9e1f775f
TranMinhPhuc <minhphuctn04@gmail.com>
2024-07-21 12:37:46 +0700

    Update code

--- src/main/java/vn/fs/Application.java
+++ src/main/java/vn/fs/Application.java
@@ -6,10 +6,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class GreenyShopApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GreenyShopApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
